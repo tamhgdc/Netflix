95244798ee8316617db7737fd5053dae926f1a19
tamhgdc <108126724+tamhgdc@users.noreply.github.com>
2022-09-21 16:01:48 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import NavBar from './Components/NavBar/NavBar'
+import './App.css'
+import {originals,action} from './urls'
+import Banner from './Components/Banner/Banner'
+import RowPost from './Components/RowPost/RowPost'
+
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+      <Banner/>
+      <RowPost url={originals} title='Netflix Originals'/>
+      <RowPost url={action} title='Action' isSmall/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/constants/constants.js
@@ -0,0 +1,6 @@
+ export const baseUrl ='https://api.themoviedb.org/3';
+ export const API_KEY ="973878d925cddeaf0ee72cc0e9b87422";
+export const imageUrl ='https://image.tmdb.org/t/p/original';
+
+
+ 
